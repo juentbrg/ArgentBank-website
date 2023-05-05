c9d5358c387caebcac89f21c4cc212afb0af1e80
juentbrg <julient.bourgeois@gmail.com>
2023-05-05 12:06:55 +0200

    correction store redux

--- FrontEnd/src/app/store.ts
+++ FrontEnd/src/app/store.ts
@@ -1,13 +1,12 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import userReducer from "../reducers/userReducer"
-
-const rootReducer = combineReducers({
-  auth: userReducer,
-});
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authSlice from "../features/authSlice";
 
 export const store = configureStore({
-  reducer: rootReducer,
-});
+  reducer: {
+    user: authSlice
+  },
+})
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
